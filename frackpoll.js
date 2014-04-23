7e1869faee4df11c235ba62ab37ae634e1bcb8f9
Alexei Nunez <anunez@sparkcognition.com>
2014-04-23 14:47:37 -0500

    litmited voting to only one button

--- frackpoll.js
+++ frackpoll.js
@@ -6,9 +6,45 @@ var SideEnum = {
   OPPONENT: 'opponent'
 };
 
+var vote = function(side, reasons) {
+  var radio = $('input[name=reasonRadio]');
+  var choice = parseInt(radio.filter(':checked').val());
+
+  var reason;
+  if (choice === reasons.length) {
+    var id = '#' + side+ 'ReasonInput'; // TODO: format string
+    reason = $(id).val();
+  }
+  else {
+    reason = reasons[choice]['value'];
+  }
+
+  console.log(side, 'reason:', reason);
+  Votes.insert({side: side, reason: reason});
+};
+
 if (Meteor.isClient) {
 
+  // Vote
+  Template.voting_template.events = {
+    'click button': function() {
+      var side = Session.get('side');
+
+      if (side === SideEnum.PROPONENT) {
+        vote(side, Template.proponents_template.reasons);
+      }
+      else if (side === SideEnum.UNDECIDED) {
+        vote(side, Template.undecided_template.reasons);
+      }
+      else if (side === SideEnum.OPPONENT) {
+        vote(side, Template.opponents_template.reasons);
+      }
+    }
+  }
+
   // Proponent
+  Template.proponents_template.side = SideEnum.PROPONENT;
+
   Template.proponents_template.proponents = function() {
     return Votes.find({side: SideEnum.PROPONENT});
   };
@@ -21,24 +57,14 @@ if (Meteor.isClient) {
   ];
 
   Template.proponents_template.events = {
-    'click button': function() {
-      var radio = $('input[name=proponentReasonRadio]');
-      var choice = parseInt(radio.filter(':checked').val());
-
-      var reason;
-      if (choice === Template.proponents_template.reasons.length) {
-        reason = $('#otherReasonInput').val();
-      }
-      else {
-        reason = Template.proponents_template.reasons[choice]['value'];
-      }
-
-      console.log('Proponent Reason:', reason);
-      Votes.insert({side: SideEnum.PROPONENT, reason: reason});
+    'click .reason': function() {
+      Session.set('side', SideEnum.PROPONENT);
     }
   };
 
   // Undecided
+  Template.undecided_template.side = SideEnum.UNDECIDED;
+
   Template.undecided_template.undecided = function() {
     return Votes.find({side: SideEnum.UNDECIDED});
   };
@@ -48,9 +74,29 @@ if (Meteor.isClient) {
     {key: 1, value: 'The evidence presented did not convince me to vote either way.' },
   ];
 
+  Template.undecided_template.events = {
+    'click .reason': function() {
+      Session.set('side', SideEnum.UNDECIDED);
+    }
+  };
+
   // Opponent
+  Template.opponents_template.side = SideEnum.OPPONENT;
+
   Template.opponents_template.opponents = function() {
     return Votes.find({side: SideEnum.OPPONENT});
+  };
+
+  Template.opponents_template.reasons = [
+    {key: 0, value: 'Water contamination concerns like methane migration.'},
+    {key: 1, value: 'Air quality concerns from large amounts of water transportation and methane emissions.'},
+    {key: 2, value: 'Fracking well pads require a lot of land which negatively effects the surrounding habitat.'},
+  ];
+
+  Template.opponents_template.events = {
+    'click .reason': function() {
+      Session.set('side', SideEnum.OPPONENT);
+    }
   };
 
 }

186314eab485f3e5a1a91d2a60d011d1060f4e57
jevi <arnunezm@yahoo.com>
2014-04-22 22:26:14 -0500

    init

---
+++ frackpoll.js
@@ -0,0 +1,27 @@
+// Collections
+Proponents = new Meteor.Collection('proponents');
+Opponents = new Meteor.Collection('opponents');
+Undecided = new Meteor.Collection('undecided');
+
+if (Meteor.isClient) {
+
+  // Templates
+  Template.proponents_list.proponents = function() {
+    return Proponents.find({}, {});
+  };
+
+  Template.undecided_list.undecided = function() {
+    return Undecided.find({}, {});
+  };
+
+  Template.opponents_list.opponents = function() {
+    return Opponents.find({}, {});
+  };
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}

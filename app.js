4e92f3a8b5281ef40e749da78edc687c2384a9c2
arnm <alexeirnunez@gmail.com>
2014-08-29 13:49:10 -0500

    removed bower components

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ $(document).ready(function() {
     $("#proponents-reasons-content").hide();
 
     var fadeAnimation = 1000;
-    
-    var onEnter = function(index) {
-	console.log('entering', index);
 
+    var showContent = function (index) {
 	if(index === 2) { $('#what-content').fadeIn(fadeAnimation); }
 	if(index === 3) { $('#how-content').fadeIn(fadeAnimation); }
 	if(index === 4) { $('#opponents-content').fadeIn(fadeAnimation); }
@@ -19,24 +17,22 @@ $(document).ready(function() {
 	if(index === 7) { $('#proponents-reasons-content').fadeIn(fadeAnimation); }
     };
 
-    var onLeave = function(index, direction) {
-	console.log('leaving', index);
-
+    var hideContent = function(index) {
 	if(index === 2) { $('#what-content').hide(fadeAnimation); }
 	if(index === 3) { $('#how-content').hide(fadeAnimation); }
 	if(index === 4) { $('#opponents-content').hide(fadeAnimation); }
 	if(index === 5) { $('#opponents-reasons-content').hide(fadeAnimation); }
 	if(index === 6) { $('#proponents-content').hide(fadeAnimation); }
 	if(index === 7) { $('#proponents-reasons-content').hide(fadeAnimation); }
+    };
+    
+    var onEnter = function(index) {
+	showContent(index);
+    };
 
-	var nextIndex;
-	if(direction === 'down') {
-	   nextIndex = index + 1; 
-	}
-	else {
-	    nextIndex= index - 1;
-	}
-	onEnter(nextIndex);
+    var onLeave = function(index, direction) {
+	hideContent(index);
+	onEnter(direction);
     };
 
     $('#fullpage').fullpage({
